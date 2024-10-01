7731690bb2d41bca56a46279d97eacb5ea0a6743
hearsay316 <hearsay316@gmail.com>
2024-10-01 11:44:04 +0800

    添加数据库初始化脚本
    添加了用于初始化 PostgreSQL 数据库的 SQL脚本，包括创建用户、聊天和消息表。还定义了聊天类型枚举，并为消息表创建了索引以提高查询性能。

---
+++ sql/init.sql
@@ -0,0 +1,40 @@
+-- this file is used for postgresql database initialization
+--create user table
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
+    fullname VARCHAR(64) NOT NULL,
+    email VARCHAR(64) NOT NULL,
+    -- hashed argon2 password
+    -- 哈希argon2密码
+    password VARCHAR(64) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+--create chat type: single, group ,private_channel, public_channel
+-- 创建聊天 type: single, group ,private_channel, public_channel
+    CREATE TYPE chat_type AS ENUM ('single', 'group', 'private_channel', 'public_channel');
+-- create chat table
+CREATE TABLE IF NOT EXISTS chats (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(128) NOT NULL UNIQUE,
+    type chat_type NOT NULL,
+    -- user id  list
+    members BIGINT[] NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- create message table
+CREATE TABLE IF NOT EXISTS messages (
+    id BIGSERIAL PRIMARY KEY,
+    chat_id BIGINT NOT NULL,
+    sender_id BIGINT NOT NULL,
+    content TEXT NOT NULL,
+    imges TEXT[],
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (chat_id) REFERENCES chats(id) ON DELETE CASCADE,
+    FOREIGN KEY (sender_id) REFERENCES users(id),
+    -- add index for chat_id
+    INDEX chat_id_idx (chat_id)
+);
+-- creat index for message for chat_id and created at order by created_at desc
+CREATE INDEX IF NOT EXISTS chat_id_created_at_index ON messages (chat_id, created_at DESC);
+
