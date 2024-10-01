efd52be760bf92eba96291be77d4c949e2df6831
hearsay316 <hearsay316@gmail.com>
2024-10-02 00:03:04 +0800

    feat: 重构项目结构并添加通知服务器- 新增 chat_server 和 notify_server 两个独立的 Cargo 工作区成员项目 - 在根目录的 Cargo.toml 中定义工作区成员和依赖 - 删除原有的 chat 项目，将其代码移至 chat_server 中 - 新增 notify_server 项目，实现基于 SSE 的通知服务器功能 - 添加 index.html 文件用于测试 SSE 连接 - 重构路由逻辑，分离聊天和通知功能

---
+++ notify_server/src/main.rs
@@ -0,0 +1,28 @@
+use anyhow::Result;
+use tokio::net::TcpListener;
+use tracing::info;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::fmt::Layer;
+use tracing_subscriber::Layer as _;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use notify_server::get_router;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    log_init();
+
+    let addr = "0.0.0.0:6687";
+    let app = get_router();
+    info!("Listener on:{}", addr);
+    let listener = TcpListener::bind(&addr).await?;
+    axum::serve(listener, app.into_make_service()).await?;
+
+    Ok(())
+}
+
+fn log_init() {
+    // let console_layer = console_subscriber::spawn(); appData  app_data
+    let layer = Layer::new().pretty().with_filter(LevelFilter::INFO);
+    tracing_subscriber::registry().with(layer).init();
+}
