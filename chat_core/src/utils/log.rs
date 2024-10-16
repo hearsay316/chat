f6b3ad59170d0af4728025dfbcce68b9cec8bf98
hearsay316 <hearsay316@gmail.com>
2024-10-16 17:21:45 +0800

    refactor:重构日志系统以提高可配置性和可读性
    - 引入类型别名 `LoggingLayer` 以简化复杂的日志层类型
    - 添加 `create_logging_layer` 函数，允许更灵活地配置日志级别
    - 维持 `local_time` 函数不变，继续使用 UTC+8 时区
    - 修改 `init_logging`函数，使用新的 `create_logging_layer` 函数初始化日志

--- chat_core/src/utils/log.rs
+++ chat_core/src/utils/log.rs
@@ -1,22 +1,32 @@
 use time::format_description::BorrowedFormatItem;
 use time::macros::format_description;
 use tracing::level_filters::LevelFilter;
+use tracing_subscriber::filter::Filtered;
+use tracing_subscriber::fmt::format::{Format, Pretty};
 use tracing_subscriber::fmt::time::OffsetTime;
 use tracing_subscriber::fmt::Layer;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
-use tracing_subscriber::Layer as _;
+use tracing_subscriber::{Layer as _, Registry};
+// 添加一个类型别名来简化复杂的类型
+pub type LoggingLayer = Filtered<
+    Layer<Registry, Pretty, Format<Pretty, OffsetTime<&'static [BorrowedFormatItem<'static>]>>>,
+    LevelFilter,
+    Registry,
+>;
 pub fn local_time() -> OffsetTime<&'static [BorrowedFormatItem<'static>]> {
     OffsetTime::new(
         time::UtcOffset::from_hms(8, 0, 0).unwrap(),
         format_description!("[year]-[month]-[day] [hour]:[minute]:[second]"),
     )
 }
-
-pub fn init_logging() {
-    let layer = Layer::new()
+fn create_logging_layer(log_config: LevelFilter) -> LoggingLayer {
+    Layer::new()
         .pretty()
         .with_timer(local_time())
-        .with_filter(LevelFilter::INFO);
+        .with_filter(log_config)
+}
+pub fn init_logging() {
+    let layer = create_logging_layer(LevelFilter::INFO);
     tracing_subscriber::registry().with(layer).init();
 }
